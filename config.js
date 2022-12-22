4d3c4965fdbbc269749f641eb0423dd60d5fc5e2
SlinkySlinky <slinkislinki@gmail.com>
2022-12-22 18:08:47 +0300

    timer added

--- config.js
+++
@@ -1,1 +0,0 @@
-export const TURN_TIME = 10
--- index.js
+++ index.js
@@ -1,42 +1,64 @@
-import express from 'express';
-import { TURN_TIME } from './config.js';
+import express, { json, text } from 'express';
+
+import fs from "fs"
 import cors from "cors"
 import setNextTurn from './functions/setNextTurn.js';
 
 
-const app = express()
-const PORT = 3001
-app.use(cors())
 
+const app = express()
+const PORT = process.env.PORT || 3001
+app.use(cors())
+app.use(text())
 
 app.listen(PORT, () => {
    console.log(`Working on ${PORT} port...`)
 })
 
+const config = JSON.parse(fs.readFileSync("./files/tradeConfig.json"))
+const turnTime = config.turnTime
+
 const serverStartTime = new Date()
 
 let turn = 1
-let startTime = Math.floor(serverStartTime.getTime() / 1000)
-let finishTime = Math.floor(serverStartTime.getTime() / 1000) + TURN_TIME
+let startTime = serverStartTime.getTime()
 
-
+const latency = 1000
+let finishTime = serverStartTime.getTime() + turnTime * 1000 + latency
+const numberOfMembers = JSON.parse(fs.readFileSync("./files/tradeConfig.json")).members.length
 
 const timer = setInterval(() => {
 
-   turn = setNextTurn(turn, 2)
+   turn = setNextTurn(turn, numberOfMembers)
    startTime = finishTime
-   finishTime = startTime + TURN_TIME
-   console.log("Turn: ", turn);
+   finishTime = startTime + turnTime * 1000 + latency
 
 
-}, TURN_TIME * 1000)
+
+}, 1000 * turnTime + latency)
 
 
 
 app.get("/getTimer", (req, res) => {
-   console.log('send: ', turn);
+
    res.send({
       turn: turn,
       finishTime: finishTime
    })
+})
+
+
+app.get("/getTrade", (req, res) => {
+
+   const data = fs.readFileSync("./files/tradeConfig.json")
+   res.send(data)
+
+})
+
+app.get("/getMembers", (req, res) => {
+
+   const data = fs.readFileSync("./files/members.json")
+
+   res.send(data)
+
 })
